67f1827f84eabd4533245b996c8bd7fd896f1a1f
kjetil midtlie <kjetil.midtlie@gmail.com>
2023-11-22 23:43:17 +0100

    figgin works

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,69 @@
 use bevy::prelude::*;
 use bevy_rapier2d::prelude::*;
-use leafwing_input_manager::prelude::*;
+use leafwing_input_manager::{plugin::InputManagerSystem, prelude::*};
+
+const PLAYER_PNG: &str = "piecePurple_border05.png";
+const MOVE_FORCE: f32 = 1500.0;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(InputManagerPlugin::<Action>::default())
+        .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(200.0))
+        .add_plugins(RapierDebugRenderPlugin::default())
+        .add_systems(Startup, setup)
+        .add_systems(Update, moveit)
+        .run();
+}
+
+// This is the list of "things in the game I want to be able to do based on input"
+#[derive(Actionlike, PartialEq, Eq, Hash, Clone, Copy, Debug, Reflect)]
+enum Action {
+    Move,
+    Fight,
+}
+
+#[derive(Component)]
+struct Player;
+
+fn setup(mut commands: Commands, assets_server: Res<AssetServer>) {
+    // entity
+    commands.spawn(Camera2dBundle::default());
+
+    //entity
+    commands
+        .spawn(SpriteBundle {
+            transform: Transform::from_xyz(-150.0, 0.0, 0.0),
+            texture: assets_server.load(PLAYER_PNG),
+            ..Default::default()
+        })
+        .insert(InputManagerBundle {
+            action_state: ActionState::default(),
+            input_map: InputMap::default()
+                .insert(VirtualDPad::wasd(), Action::Move)
+                .build(),
+        })
+        // physics from rapier2d
+        .insert(RigidBody::Dynamic)
+        .insert(Collider::ball(32.0))
+        .insert(ExternalForce {
+            force: Vec2::ZERO,
+            torque: 0.0,
+        })
+        .insert(Damping {
+            linear_damping: 0.6,
+            angular_damping: 5.0,
+        })
+        .insert(Restitution::coefficient(0.8))
+        .insert(Player);
+}
+
+fn moveit(
+    mut query: Query<(&ActionState<Action>, &mut ExternalForce), With<Player>>,
+    time: Res<Time>,
+) {
+    for (a_state, mut ext_force) in &mut query {
+        let axis_vector = a_state.clamped_axis_pair(Action::Move).unwrap().xy();
+        ext_force.force = axis_vector * MOVE_FORCE * time.delta_seconds();
+    }
 }
